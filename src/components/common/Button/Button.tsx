1725827760061e0af71a55c0402ee966428a3c9a
Minsoo Cho <textign@gmail.com>
2024-07-09 00:14:52 +0900

    update: 버튼 css 업데이트

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,7 +1,9 @@
 import { PropsWithChildren } from "react";
 
 function Button({ children }: PropsWithChildren) {
-  return <span className="rounded py-2 px-4 bg-[#24CAFF] border-[#00BBF7] text-center text-white">{children}</span>;
+  return (
+    <span className="rounded py-2 px-4 bg-[#24CAFF] border-[#00BBF7] text-center text-white font-bold">{children}</span>
+  );
 }
 
 export default Button;
