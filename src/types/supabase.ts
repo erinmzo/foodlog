5d977dfce8c7c0105ee172fa586445d1a120ad5a
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-07-09 20:06:15 +0900

    update: supabase 변경

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -5,6 +5,7 @@ export type Database = {
     Tables: {
       posts: {
         Row: {
+          address: string | null;
           category: string;
           content: string;
           created_at: string;
@@ -18,6 +19,7 @@ export type Database = {
           user_nickname: string;
         };
         Insert: {
+          address?: string | null;
           category: string;
           content: string;
           created_at?: string;
@@ -31,6 +33,7 @@ export type Database = {
           user_nickname: string;
         };
         Update: {
+          address?: string | null;
           category?: string;
           content?: string;
           created_at?: string;
