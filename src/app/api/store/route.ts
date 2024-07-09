b9e187d42b539121b93e77f248b91aaba3e2da4c
USERMIC-Q8MSLVD\Administrator <sssb2046@gmail.com>
2024-07-09 19:28:41 +0900

    hh

--- src/app/api/store/route.ts
+++ src/app/api/store/route.ts
@@ -4,34 +4,30 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET() {
   try {
     const supabase = createClient();
-    const { data, error } = await supabase.from("posts").select("*");
-    
-    if (error) {
-      throw error;
-    }
-
+    const { data } = await supabase.from("posts").select("*");
     return NextResponse.json(data);
   } catch (error) {
-    console.error(error);
     return NextResponse.json({ error: "데이터를 가져오는 데 실패했습니다." });
   }
 }
 
 export async function POST(request: NextRequest) {
-  try {
-    const body = await request.json(); // 요청 본문에서 데이터 추출
+  try{
     const supabase = createClient();
-    const { data, error } = await supabase.from("posts").insert([body]).select();
-
-    if (error) {
-      console.error(error);
-      return NextResponse.json({ error: error.message });
-    }
-
-    return NextResponse.json(data);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "데이터를 삽입하는 데 실패했습니다." });
+    const { data, error } = await supabase.from("posts").insert(request)
+  .select();
+  
+  if(error) {
+    console.log(error);
+    return alert(`${error.message}`);
+    
+    
+  }
+  return NextResponse.json(data);
+  }catch (error) {
+    console.log(4);
+    
+  return NextResponse.json({ error: "데이터를 가져오는 데 실패했습니다." });
   }
 }
 
