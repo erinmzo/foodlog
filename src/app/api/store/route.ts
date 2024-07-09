667c04af0cde2f8dd20f773c7be380908e8d444c
USERMIC-Q8MSLVD\Administrator <sssb2046@gmail.com>
2024-07-09 19:48:28 +0900

    fix: writepage supabase 연동완료

--- src/app/api/store/route.ts
+++ src/app/api/store/route.ts
@@ -1,36 +1,35 @@
-import { createClient } from "@/supabase/server";
-import { NextRequest, NextResponse } from "next/server";
+import { createClient } from '@/supabase/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET() {
   try {
-    const supabase = createClient();
-    const { data } = await supabase.from("posts").select("*");
-    return NextResponse.json(data);
+    const supabase = createClient()
+    const { data } = await supabase.from('posts').select('*')
+    return NextResponse.json(data)
   } catch (error) {
-    return NextResponse.json({ error: "데이터를 가져오는 데 실패했습니다." });
+    return NextResponse.json({ error: '데이터를 가져오는 데 실패했습니다.' })
   }
 }
 
 export async function POST(request: NextRequest) {
-  try{
-    const supabase = createClient();
-    const { data, error } = await supabase.from("posts").insert(request)
-  .select();
-  
-  if(error) {
-    console.log(error);
-    return alert(`${error.message}`);
-    
-    
-  }
-  return NextResponse.json(data);
-  }catch (error) {
-    console.log(4);
-    
-  return NextResponse.json({ error: "데이터를 가져오는 데 실패했습니다." });
+  console.log('🚀 ~ POST ~ request:', request)
+  try {
+    const tt = await request.json()
+    const supabase = createClient()
+    const { data, error } = await supabase.from('posts').insert(tt).select()
+
+    if (error) {
+      console.log(22, error)
+      return alert(`${error.message}`)
+    }
+    return NextResponse.json(data)
+  } catch (error) {
+    console.log(error)
+
+    return NextResponse.json({ error: '데이터를 가져오는 데 실패했습니다.' })
   }
 }
 
 export async function DELETE(request: NextRequest) {
-  return NextResponse.json("");
+  return NextResponse.json('')
 }
--- src/components/post/write/Write.tsx
+++ src/components/post/write/Write.tsx
@@ -1,54 +1,53 @@
-"use client";
+'use client'
 
-import Button from "@/components/common/Button";
-import { Post } from "@/types/store";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useRef } from "react";
-
+import Button from '@/components/common/Button'
+import { Post } from '@/types/store'
+import { useMutation } from '@tanstack/react-query'
+import { useRef } from 'react'
 
 function WritePage() {
-  const categoryRef = useRef<HTMLInputElement>(null);
-  const storeRef = useRef<HTMLInputElement>(null);
-  const menuRef = useRef<HTMLInputElement>(null);
-  const orderDateRef = useRef<HTMLInputElement>(null);
-  const userRef = useRef<HTMLInputElement>(null);
-  const addressRef = useRef<HTMLInputElement>(null);
-  const ratingRef = useRef<HTMLInputElement>(null);
-  const imageRef = useRef<HTMLInputElement>(null);
-  const contentRef = useRef<HTMLInputElement>(null);
+  const categoryRef = useRef<HTMLInputElement>(null)
+  const storeRef = useRef<HTMLInputElement>(null)
+  const menuRef = useRef<HTMLInputElement>(null)
+  const orderDateRef = useRef<HTMLInputElement>(null)
+  const userRef = useRef<HTMLInputElement>(null)
+  const addressRef = useRef<HTMLInputElement>(null)
+  const ratingRef = useRef<HTMLInputElement>(null)
+  const imageRef = useRef<HTMLInputElement>(null)
+  const contentRef = useRef<HTMLInputElement>(null)
 
-
-  const addStoreList = async (data)=> {
-    const response = await fetch("http://localhost:3000/api/store", {
-      method: "POST",
-      body: data
+  const addStoreList = async (data) => {
+    const response = await fetch('http://localhost:3000/api/store', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
     })
-    console.log(data);
-    console.log(response);
-    
-    return response.json();
+    console.log(1, data)
+    console.log(2, response)
 
-    
+    return response.json()
   }
 
-  const {mutate: addMutation} = useMutation<Post>({
-    mutationFn: (data)=>addStoreList(data)
+  const { mutate: addMutation } = useMutation<Post>({
+    mutationFn: (data) => addStoreList(data),
   })
 
-  const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
     const postData = {
-      category: categoryRef.current?.value || "",
-      store_name: storeRef.current?.value || "",
-      menu_name: menuRef.current?.value || "",
-      order_date: orderDateRef.current?.value || "",
-      address: addressRef.current?.value || "",
-      rating: ratingRef.current?.value || "",
-      content: contentRef.current?.value || "",
-      img_url: imageRef.current?.value || "",
+      category: categoryRef.current?.value || '',
+      store_name: storeRef.current?.value || '',
+      menu_name: menuRef.current?.value || '',
+      order_date: orderDateRef.current?.value || '',
+      address: addressRef.current?.value || '',
+      rating: ratingRef.current?.value || '',
+      content: contentRef.current?.value || '',
+      img_url: imageRef.current?.value || '',
+      user_nickname: '몰라',
+      user_id: 'fa77e563-3245-4654-a240-b064703ab2ca',
     }
-    console.log(postData);
-    addMutation(postData);
+    // console.log(postData)
+    addMutation(postData)
   }
 
   return (
@@ -61,64 +60,56 @@ function WritePage() {
           식당과 메뉴를 공유해주세요!
         </h3>
 
-        <form 
+        <form
           className="bg-blue-100 w-full p-20 rounded-xl shadow-lg shadow-gray-500/20"
-          onSubmit={onSubmit}>
+          onSubmit={onSubmit}
+        >
           <div className="flex w-full mt-5 items-center">
-            <label className="w-[10%] whitespace-nowrap">
-              유형
-            </label>
-            <select className="w-[10%] p-2 border rounded-md"  ref={categoryRef}>
+            <label className="w-[10%] whitespace-nowrap">유형</label>
+            <select className="w-[10%] p-2 border rounded-md" ref={categoryRef}>
               <option value="visited">방문</option>
               <option value="delivery">배달</option>
             </select>
           </div>
 
           <div className="flex mt-5 items-center">
-            <label className="w-[10%] whitespace-nowrap mr-2">
-              식당이름
-            </label>
+            <label className="w-[10%] whitespace-nowrap mr-2">식당이름</label>
             <input
               className="w-[40%] p-2 border rounded-md mr-10"
               type="text"
               ref={storeRef}
             />
-            <label className="w-[10%] whitespace-nowrap mr-2">
-              메뉴이름
-            </label>
-            <input className="w-[40%] p-2 border rounded-md" type="text"  ref={menuRef}/>
+            <label className="w-[10%] whitespace-nowrap mr-2">메뉴이름</label>
+            <input
+              className="w-[40%] p-2 border rounded-md"
+              type="text"
+              ref={menuRef}
+            />
           </div>
 
           <div className="flex mt-5 items-center">
-            <label
-              className="w-[10%] whitespace-nowrap mr-2"
-              
-            >
-              주문날짜
-            </label>
+            <label className="w-[10%] whitespace-nowrap mr-2">주문날짜</label>
             <input
               className="w-[40%] p-2 border rounded-md mr-10"
               type="date"
               ref={orderDateRef}
             />
-            <label className="w-[10%] whitespace-nowrap mr-2">
-              작성자
-            </label>
-            <input className="w-[40%] p-2 border rounded-md" type="text"  ref={userRef}/>
+            <label className="w-[10%] whitespace-nowrap mr-2">작성자</label>
+            <input
+              className="w-[40%] p-2 border rounded-md"
+              type="text"
+              ref={userRef}
+            />
           </div>
 
           <div className="flex mt-5 items-center">
-            <label className="w-[10%] whitespace-nowrap mr-2" >
-              주소
-            </label>
+            <label className="w-[10%] whitespace-nowrap mr-2">주소</label>
             <input
               className="w-[40%] p-2 border rounded-md mr-10"
               type="text"
               ref={addressRef}
             />
-            <label className="w-[10%] whitespace-nowrap mr-2">
-              별점
-            </label>
+            <label className="w-[10%] whitespace-nowrap mr-2">별점</label>
             <select className="w-[40%] p-2 rounded-md" ref={ratingRef}>
               <option value="first">1</option>
               <option value="second">1.5</option>
@@ -134,7 +125,11 @@ function WritePage() {
 
           <div className="flex mt-5 items-center">
             <label className="w-[10%] whitespace-nowrap">이미지</label>
-            <input className="w-[90%] bg-white p-2 rounded-md" type="file" ref={imageRef}/>
+            <input
+              className="w-[90%] bg-white p-2 rounded-md"
+              type="file"
+              ref={imageRef}
+            />
           </div>
 
           <div className="mt-5">
@@ -147,12 +142,12 @@ function WritePage() {
           </div>
 
           <div className="mt-5 text-right">
-              <Button>작성하기</Button>
+            <Button>작성하기</Button>
           </div>
         </form>
       </div>
     </>
-  );
+  )
 }
 
-export default WritePage;
+export default WritePage
