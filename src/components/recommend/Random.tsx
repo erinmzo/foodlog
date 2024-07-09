a964a8030de6b471c2abb80803f21aca782c1c0e
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-07-09 19:25:36 +0900

    style: 랜덤 음식 추천 style 수정

--- src/components/recommend/Random.tsx
+++ src/components/recommend/Random.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 
 interface Food {
   id: number;
@@ -21,30 +21,51 @@ const Random = () => {
   const { data: random, refetch } = useQuery<Food>({
     queryKey: ["getRandomFood"],
     queryFn: fetchRandomFood,
+    enabled: false,
   });
 
-  const handleClick = () => {
-    refetch();
-  };
+  const [showFood, setShowFood] = useState(false);
 
+  const handleClick = async () => {
+    await refetch();
+    setShowFood(true);
+  };
   return (
-    <div className="flex flex-col items-center mb-[50px]">
+    <div className="w-2/3 flex flex-col items-center mx-auto">
       <h3 className="text-2xl font-bold">오늘 뭐 먹지?</h3>
       <p className="text-[16px] text-[#878787] mt-2 mb-4">
         클릭해보세요 메뉴를 추천해드립니다
       </p>
-      <button
-        className="w-1/3 py-20 text-5xl font-bold text-[#24CAFF] border rounded-md"
-        onClick={handleClick}
-      >
-        Click!!
-      </button>
-      {random && (
-        <div className="flex flex-col items-center mt-8">
-          <h2 className="text-5xl font-bold text-[#24CAFF]">{random.menu}</h2>
-          <img src={random.img_url} alt={random.menu} />
-        </div>
-      )}
+      <div className="w-full flex flex-col items-center">
+        {!showFood && (
+          <button
+            className="w-1/2 py-8 text-2xl font-bold text-[#24CAFF] border rounded-md mb-8"
+            onClick={handleClick}
+          >
+            Click!!
+          </button>
+        )}
+        {showFood && random && (
+          <div className="w-full h-full flex flex-col items-center mt-8">
+            <h2 className="text-5xl font-bold text-[#24CAFF] mb-4">
+              {random.menu}
+            </h2>
+            <div className="w-1/2 max-h-[200px] flex items-center justify-center overflow-hidden rounded-lg">
+              <img
+                src={random.img_url}
+                alt={random.menu}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <button
+              className="w-1/2 py-2 text-2xl font-bold text-[#24CAFF] border rounded-md my-6"
+              onClick={handleClick}
+            >
+              다시 하기
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
