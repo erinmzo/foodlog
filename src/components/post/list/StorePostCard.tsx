40ee93e82206685249ee89d421f98c6dd395b1a0
Minsoo Cho <textign@gmail.com>
2024-07-10 13:44:18 +0900

    update: 메인 리스트 이미지 노출

--- src/components/post/list/StorePostCard.tsx
+++ src/components/post/list/StorePostCard.tsx
@@ -6,24 +6,18 @@ interface StorePostCardProps {
 }
 
 function StorePostCard({ post }: StorePostCardProps) {
+  console.log(post.img_url);
   return (
     <div className="flex flex-col rounded-[15px] overflow-hidden border border-[#f5f5f5]">
       <div className="relative aspect-video">
-        <Image
-          src="/img/test-img.png"
-          alt={post.menu_name}
-          fill
-          className="object-fill"
-        />
+        {post.img_url && (
+          <Image src={post.img_url} alt={post.menu_name} fill sizes="328px" className="object-cover" priority />
+        )}
       </div>
       <div className="p-[12px] bg-[#fdfdfd]">
         <div className="text-[13px] text-[#878787]">{post.order_date}</div>
-        <h4 className="text-[18px] font-bold mt-2 truncate">
-          {post.store_name}
-        </h4>
-        <div className="text-[#24CAFF] font-bold mt-2 truncate">
-          {post.menu_name}
-        </div>
+        <h4 className="text-[18px] font-bold mt-2 truncate">{post.store_name}</h4>
+        <div className="text-[#24CAFF] font-bold mt-2 truncate">{post.menu_name}</div>
         <div className="mt-2 truncate">{post.content}</div>
         <div className="flex justify-between mt-6 text-[13px] text-[#878787]">
           <div>by. {post.user_nickname}</div>
