9cdff3822f43b6ed733c5f290ccbb6878dac60e3
airtrader <hosi2714@naver.com>
2024-07-10 09:35:05 +0900

    style: 버튼 css 추가 및 모달css 수정

--- src/components/mypage/list/MyPageList.tsx
+++ src/components/mypage/list/MyPageList.tsx
@@ -25,8 +25,9 @@ function MyPageList() {
   }
 
   return (
-    <div className="container mx-auto max-w-[1024px]">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[17px]">
+    <div className="container mx-auto max-w-[1024px] mt-24">
+      <h1 className="text-center text-2xl font-bold">나의 푸드로그</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[17px] mt-16">
         {posts.map((post) => (
           <Link key={post.id} href={`post/read/${post.id}`}>
             <StorePostCard post={post} />
--- src/components/mypage/profile/modal/Modal.tsx
+++ src/components/mypage/profile/modal/Modal.tsx
@@ -16,23 +16,34 @@ const Modal = (props: Props) => {
   };
   return (
     <div
-      className="fixed top-0 left-0 w-full h-full flex my-auto justify-center items-center bg-slate-200/90 "
+      className="fixed top-0 left-0 w-full h-full flex my-auto justify-center items-center bg-[#00000066]/40 "
       onClick={clickModal}
     >
       <div
-        className="border-2 border-black bg-white w-96 h-64 rounded-lg flex flex-col my-auto justify-center items-center"
+        className="bg-white w-[462px] h-[266px] rounded-[30px] flex flex-col my-auto justify-center items-center"
         onClick={(e) => e.stopPropagation()}
       >
         <h1 className="text-2xl font-bold">프로필 수정</h1>
         <input
-          className="border-2 border-black rounded-lg flex items-center text-center mt-5 "
+          className="border-2 border-[#D2D2D2] w-[184px] h-[45px] rounded-lg flex items-center text-center mt-5 "
           type="text"
           placeholder="변경할 닉네임"
           onChange={handleChange}
         />
         <div className="mt-5">
-          <button onClick={clickModal}>뒤로가기</button>
-          <button onClick={clickChange}>변경하기</button>
+          <button
+            className="rounded py-2 px-4 bg-[#24CAFF] border-[#00BBF7] text-center text-white font-bold"
+            onClick={clickModal}
+          >
+            뒤로가기
+          </button>
+          &nbsp;&nbsp;
+          <button
+            className="rounded py-2 px-4 bg-[#24CAFF] border-[#00BBF7] text-center text-white font-bold"
+            onClick={clickChange}
+          >
+            변경하기
+          </button>
         </div>
       </div>
     </div>
