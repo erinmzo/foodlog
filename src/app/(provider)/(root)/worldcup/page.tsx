ccda188f8d36b926af5de52905fa7c0bae79cd56
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-07-10 12:21:49 +0900

    add: 월드컵 페이지 생성

---
+++ src/app/(provider)/(root)/worldcup/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WorldCupPage = () => {
+  return (
+    <div>WorldCupPage</div>
+  )
+}
+
+export default WorldCupPage
---
+++ src/components/recommend/FoodWorldCup.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FoodWorldCup = () => {
+  return (
+    <div>FoodWorldCup</div>
+  )
+}
+
+export default FoodWorldCup
