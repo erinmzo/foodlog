f54135db369dbfb240cafee46ec8ac7763c109ec
Minsoo Cho <textign@gmail.com>
2024-07-12 12:53:54 +0900

    / 빼먹음

--- src/components/post/list/StorePostList.tsx
+++ src/components/post/list/StorePostList.tsx
@@ -25,7 +25,7 @@ function StorePostList() {
     <div className="container mx-auto max-w-[1024px]">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[17px] px-5 lg:px-0">
         {posts.map((post) => (
-          <Link key={post.id} href={`post/read/${post.id}`}>
+          <Link key={post.id} href={`/post/read/${post.id}`}>
             <StorePostCard post={post} />
           </Link>
         ))}
