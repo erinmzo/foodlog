fd91fe92812c2c3b80ac06d40592da5dbb6e124d
Minsoo Cho <textign@gmail.com>
2024-07-12 12:38:01 +0900

    랜덤 쪽 수정

--- src/app/api/recommend/route.ts
+++ src/app/api/recommend/route.ts
@@ -1,20 +1,16 @@
 import { createClient } from "@/supabase/server";
 import { NextResponse } from "next/server";
 
-export const getRandomFood = async () => {
+export async function GET() {
   const supabase = createClient();
   const { data, error } = await supabase.from("recommend").select("*");
   if (error) {
     return null;
   }
   const randomIndex = Math.floor(Math.random() * data.length);
-  return data[randomIndex];
-};
 
-export async function GET() {
-  const randomFood = await getRandomFood();
-  if (!randomFood) {
+  if (!data[randomIndex]) {
     return NextResponse.json({ error: "데이터 가져오기 실패" }, { status: 404 });
   }
-  return NextResponse.json(randomFood);
+  return NextResponse.json(data[randomIndex]);
 }
