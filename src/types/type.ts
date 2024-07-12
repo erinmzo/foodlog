5f07d737273bcf70ed950d9238b23f1f85b37524
Minsoo Cho <textign@gmail.com>
2024-07-12 16:05:44 +0900

    최종 커밋

--- src/types/type.ts
+++ src/types/type.ts
@@ -9,17 +9,3 @@ export type Comments = Tables<"comments">;
 export type Recommend = Tables<"recommend">;
 
 export type TPostData = Omit<Post, "created_at" | "id">;
-
-// {
-//   category: string;
-//   store_name: string;
-//   menu_name: string;
-//   order_date: string;
-//   address: string;
-//   rating: string;
-//   content: string;
-//   img_url: string;
-//   user_nickname: string;
-//   user_id: string;
-//   id?: string;
-// }
